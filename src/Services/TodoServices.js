f69b242b70c996889abb25f1c1195951483361c5
siddharthyadava <siddharthyadav2904@gmail.com>
2025-11-30 14:27:37 +0530

    Fixec todo services

--- src/Services/TodoServices.js
+++ src/Services/TodoServices.js
@@ -1,30 +1,52 @@
 import axios from "axios";
 
-//get user token
-const user = JSON.parse(localStorage.getItem("todoapp"));
-
+// BASE URL (Render ya Local)
 const baseUrl = process.env.REACT_APP_BASEURL;
 
-//default auth header
-axios.defaults.headers.common["Authorization"] = `bearer ${user.token}`;
+// 🔐 Safe function to get token (without crashing)
+const getToken = () => {
+  try {
+    const raw = localStorage.getItem("todoapp");
+    if (!raw) return null;
 
-//CRETE TODO
+    const userData = JSON.parse(raw);
+
+    // userData.token OR userData.user.token
+    return userData?.token || userData?.user?.token || null;
+  } catch (err) {
+    console.error("Error reading user token:", err);
+    return null;
+  }
+};
+
+// 🔐 Token lagane ka safe method
+axios.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// CREATE TODO
 const createTodo = (data) => {
   return axios.post(`${baseUrl}/todo/create`, data);
 };
-//GET ALL TODO
-const getAllTodo = (id) => {
-  return axios.post(`${baseUrl}/todo/getAll/${id}`);
+
+// GET ALL TODO
+const getAllTodo = (userId) => {
+  return axios.post(`${baseUrl}/todo/getAll`, { userId });
 };
 
-//UPDATE TODO
+// UPDATE TODO
 const updateTodo = (id, data) => {
-  return axios.patch(`${baseUrl}/todo/update/` + id, data);
+  return axios.patch(`${baseUrl}/todo/update/${id}`, data);
 };
 
-//DLEETE TODO
+// DELETE TODO
 const deleteTodo = (id) => {
-  return axios.delete(`${baseUrl}/todo/delete/` + id);
+  return axios.delete(`${baseUrl}/todo/delete/${id}`);
 };
 
 const TodoServices = { createTodo, getAllTodo, updateTodo, deleteTodo };
